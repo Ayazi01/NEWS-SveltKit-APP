cf10c74fc3c191d911d28084be8a541e1a9019f8
marou <marou@admin2.local>
2023-02-09 09:56:04 -0500

    working on accessibility of secret api key

--- src/routes/api/news/+server.ts
+++ src/routes/api/news/+server.ts
@@ -4,7 +4,7 @@ import { SECRET_API_KEY } from '$env/static/private';
 import type { Media } from '../../../lib/interfaces/posts.interface';
 
 export const GET: RequestHandler = async () => {
-	const resourceUrl = `http://api.mediastack.com/v1/news?countries=us,in&'sort=published_desc&&sources=en&access_key=${SECRET_API_KEY? SECRET_API_KEY : ""}`;
+	const resourceUrl = `http://api.mediastack.com/v1/news?countries=us,in&'sort=published_desc&&sources=en&access_key=${process.env.SECRET_API_KEY}`;
 	const res = await fetch(resourceUrl);
 	const posts: Media = await res.json();
 
